1d9583efa3c7d97a65ee7af3ebde7e4e23d0b571
EmmaC <c.emma2112@gmail.com>
2023-05-17 19:23:19 +0000

    add tuple to determine post status

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+
+
 # Create your models here.
+
+#creating tuple for status, whether post is draft or published
+STATUS = ((0, "Draft"), (1, "Published"))
