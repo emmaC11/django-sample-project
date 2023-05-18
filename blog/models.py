ebc78c436db3b52b50f513f03eab25e6ecd00531
EmmaC <c.emma2112@gmail.com>
2023-05-18 07:36:40 +0000

    add comments model

--- blog/models.py
+++ blog/models.py
@@ -38,3 +38,19 @@ class Post(models.Model):
         return self.likes.count()
 
 
+class Comments(models.Model):
+
+    #post field is a one to many relationship, as one post can have many comments
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        #ascending order
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
