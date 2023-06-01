7944db6680f1a8512680f4834818315a93aeb336
EmmaC <c.emma2112@gmail.com>
2023-06-01 18:56:11 +0000

    updat text ield to summernote field in admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
+
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content')
+    #saying our content field is a summernote field, previously a django text field
+
 
 admin.site.register(Post)
