de42342b284b30077ba019ae2ae1af54609e1523
EmmaC <c.emma2112@gmail.com>
2023-06-01 19:39:34 +0000

    add additional admin functionalties

--- blog/admin.py
+++ blog/admin.py
@@ -6,6 +6,8 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 
 class PostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
     prepopulated_fields = {'slug':('title',)}
     summernote_fields = ('content')
     #saying our content field is a summernote field, previously a django text field
