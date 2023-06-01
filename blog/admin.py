ecbf513429664b98a6028d2c0d83e680ad48cdca
EmmaC <c.emma2112@gmail.com>
2023-06-01 18:59:39 +0000

    replace register call with decorator

--- blog/admin.py
+++ blog/admin.py
@@ -3,9 +3,11 @@ from .models import Post
 from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
 
+@admin.register(Post)
+
 class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('content')
     #saying our content field is a summernote field, previously a django text field
 
 
-admin.site.register(Post)
+#admin.site.register(Post)
