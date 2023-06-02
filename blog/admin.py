64c8a714f7049d7cc09785d0205da440aa82c618
EmmaC <c.emma2112@gmail.com>
2023-06-02 21:12:51 +0000

    add filter to post admin section

--- blog/admin.py
+++ blog/admin.py
@@ -7,6 +7,7 @@ from django_summernote.admin import SummernoteModelAdmin
 
 class PostAdmin(SummernoteModelAdmin):
     list_display = ('title', 'slug', 'status', 'created_on')
+    list_filter = ('status','created_on')
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug':('title',)}
     summernote_fields = ('content')
