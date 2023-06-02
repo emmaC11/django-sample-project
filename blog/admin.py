4cd3b3ce9fd282df35ab183c6783fda40cf1c09e
EmmaC <c.emma2112@gmail.com>
2023-06-02 20:53:54 +0000

    add admin comment class & decorator

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comments
 from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
 
@@ -12,5 +12,12 @@ class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('content')
     #saying our content field is a summernote field, previously a django text field
 
+
+@admin.register(Comments)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
 
 #admin.site.register(Post)
