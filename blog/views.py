108e1b5c0e94f29c18eebc06001f8ffb58c7ce35
EmmaC <c.emma2112@gmail.com>
2023-06-10 11:14:16 +0000

    fix typo in PostDetail view

--- blog/views.py
+++ blog/views.py
@@ -22,7 +22,7 @@ class PostDetail(View):
         # to identify a specific post we can use the slug param, which is unique for each blog post
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by('creted_on')
+        comments = post.comments.filter(approved=True).order_by('created_on')
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
